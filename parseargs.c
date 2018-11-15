1471ecbd62c1f57bb507ad7a4d0fc5d8a58f59e5
horazio <hfusco@stuy.edu>
2018-11-14 21:38:03 -0500

    basic algorithm works, still unclear about the random error

--- parseargs.c
+++ parseargs.c
@@ -5,14 +5,15 @@
 
 char **parse_args( char *line )
 {
-    char s[256];
-    strcpy( s, line);
+    //char s[256];
+    //strcpy( s, line);
+
     char **args = calloc(8, sizeof(char *)); 
     int i = 0;
-    while( s ) {
-        printf("it's OOF\n");
-        args[i] = strsep( &s, " " );
-        printf("OOF %s\n", args[i]);
+    while( line ) {
+        //printf("it's OOF\n");
+        args[i] = strsep( &line, " " );
+        //printf("OOF %s\n", args[i]);
         i++;
     }
     return args;
@@ -20,10 +21,12 @@ char **parse_args( char *line )
 
 int main()
 {
+   
     char in[] = "ls -a -l ..";
+    printf("the line is: \"%s\"\nParsed:\n", in);
     char **jeff = parse_args(in);
     int i = 0;
-    printf("OOF\n");
+    //printf("OOF\n");
     while( jeff[i] )
         printf("\"%s\"\n", jeff[i++]);
     free(jeff);
